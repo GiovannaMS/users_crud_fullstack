b7b1e02ee2a353598d9d24440a2a1c00ec0ea017
GiovannaMS <giovannams3999@gmail.com>
2024-02-08 22:56:40 -0300

    api routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { getUsers } from "../controllers/user.js";
 
-const router = express.Router();
+const router = express.Router(); //cria uma rota
 
-router.get("/", getUsers);
+router.get("/", getUsers); //Como nao vai ter nenhuma rota por ser um projeto simples, informa que começa na raiz do prjeto e chama o método getUsers
 
-export default router;
+export default router; //exporta a rota criada
