e675387c9e429f0888cc038c8f36e695eade6f9b
GiovannaMS <giovannams3999@gmail.com>
2024-02-21 23:14:55 -0300

    feat: frontend creation

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,13 +1,12 @@
 import { db } from "../db.js";
 
-export const getUsers = (_, res) => { //_ seria o request enviado para a método getUser, no caso não se aplica
-                                      //res é a resposta do método
-    const q = "SELECT * FROM users"; //comando sql q a query vai executar no banco
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM users";
     const response = (err, data) => {
-        if (err) return res.json(err); //se houver erro retorna um json com o erro
+        if (err) return res.json(err);
 
-        return res.status(200).json(data); //se não houver erro retorna status code de 200 e um json com os usuários
+        return res.status(200).json(data); 
     };
 
-    db.query(q, response); //cria query passando o comando sql e a função para tratar a resposta
+    db.query(q, response);
 };
